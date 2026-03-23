c3fa54605c23b7d75bb1a3b9c28790868d1bf136
Brian Abbott <hi@brianabbott.com>
2026-03-23 10:34:58 -0400

    create Gradle Project with JavaFX as a dependancy

---
+++ src/main/java/HelloFX.java
@@ -0,0 +1,32 @@
+/**
+ * File: HelloJavaFx.java
+ * Class: CSCI 1302
+ * Author: Brian Abbott
+ * Created on: 3/23/26
+ * Last modified: 3/23/26
+ * Description: To be filled in by the user.
+ */
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloFX extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
